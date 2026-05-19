4b1c1355b67dad71ce4d3c6cae830619fc18c024
web-gkumar <kgautam431@gmail.com>
2026-05-19 14:44:35 +0530

    delete request fixing

--- server/models/order.js
+++ server/models/order.js
@@ -3,10 +3,11 @@ const mongoose = require("mongoose");
 const FileSchema = new mongoose.Schema({ url:String, type:String});
 const OrderSchema = new mongoose.Schema(
 {
-  purpose: { type: String, enum: ["buy", "sell"], required: true},
-  cropName: { type: String, required: true},
-  price: { type: String, required: true},
-  quantity: { type: String, required: true},
+  purpose: { type: String, enum: ["buy", "sell"]},
+  mobile: { type: String },
+  cropName: { type: String},
+  price: { type: String},
+  quantity: { type: String},
   deliveryDate: { type: Date},
   files: [FileSchema]
 },
